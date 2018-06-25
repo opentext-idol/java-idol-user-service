7b147e0699b3cee35f9e2db671b6cf8dfa444e98
Tung Jin Chew <tung-jin.chew@hpe.com>
2018-06-25 16:17:16 +0100

    (CORE-5213) Create 2.1.0 branch for Site Admin with the recurse code

--- travis.sh
+++ travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ ${TRAVIS_BRANCH} == 'master' || ${TRAVIS_BRANCH} == 'develop' ]]
+if [[ ${TRAVIS_BRANCH} == 'master' || ${TRAVIS_BRANCH} == 'develop'  || ${TRAVIS_BRANCH} == 'release/2.1.0' ]]
 then
   echo "Deploying Jar to Maven Central"
   mvn deploy -DskipTests --settings settings.xml -Prelease
