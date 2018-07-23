fa3d2d586688b3af8e857a7fc329d9c63259b730
Tung Jin Chew <tung-jin.chew@hpe.com>
2018-07-23 16:43:04 +0100

    (ISA-158) We want to deploy the 2.1.1 branch to Travis

--- travis.sh
+++ travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ ${TRAVIS_BRANCH} == 'master' || ${TRAVIS_BRANCH} == 'develop' ]]
+if [[ ${TRAVIS_BRANCH} == 'master' || ${TRAVIS_BRANCH} == 'develop' || ${TRAVIS_BRANCH} == '2.1.1-paged' ]]
 then
   echo "Deploying Jar to Maven Central"
   mvn deploy -DskipTests --settings settings.xml -Prelease
